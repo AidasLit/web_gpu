d8df3e6a43ac0923e8b5bd20df7e2f5f1eaa5447
AidasLit <aidaslit@gmail.com>
2026-01-10 00:25:00 +0200

    added exported app files

---
+++ pkg/web_gpu_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __wasm_bindgen_func_elem_7782: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_7562: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_9506: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_9505: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_7789: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_7784: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
